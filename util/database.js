287f16003ce413e63da9cde1bd1c6ae778fe73c5
Zoran <zoranstankovic24@gmail.com>
2019-11-24 19:45:21 +0100

    Changed Mongo DB URI
    Changed Mongo DB URI

--- util/database.js
+++ util/database.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
-const dbURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-jquza.mongodb.net/flash?retryWrites=true&w=majority`;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(dbURI, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false
